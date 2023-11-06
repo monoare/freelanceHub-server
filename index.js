102062dd46c636928e29b02c9da9495510907558
ProgrammingHero1 <mamun2k9kuet@gmail.com>
2023-11-06 18:18:39 +0600

    added cors middleware

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qfcfzds.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
