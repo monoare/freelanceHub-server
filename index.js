de299f925a0bda410dc49c6f69b738c6ac08e558
ProgrammingHero1 <mamun2k9kuet@gmail.com>
2023-11-06 18:00:12 +0600

    get the data from the database accroding to category

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const uri = `mongodb+srv://${process.env.DB_USER}>:${process.env.DB_USER}@cluster0.qfcfzds.mongodb.net/?retryWrites=true&w=majority`;
+// middleware
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qfcfzds.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -18,7 +21,36 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const jobsCollection = client.db("freelanceHubDB").collection("jobs");
+
+    //   Web Development related ServerApiVersion
+    app.get("/api/v1/jobs", async (req, res) => {
+      let queryObj = {};
+      const category = req.query.category;
+      console.log({ category });
+      if (category) {
+        queryObj.category = category;
+      }
+      const cursor = jobsCollection.find(queryObj);
+      const result = await cursor.toArray();
+      console.log({ result });
+      res.send(result);
+    });
+
+    // app.get("/api/v1/jobs/category", async (req, res) => {
+    //   const category = req.body.category;
+    //   let query = {};
+    //   if (category) {
+    //     query.category = category;
+    //   }
+    //   console.log({ query });
+    //   const result = await jobsCollection.find(query).toArray();
+    //   console.log({ result });
+    //   res.send(result);
+    // });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -26,7 +58,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
